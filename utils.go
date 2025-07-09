8861b61033ce258ecccad520447323bf5eb7511f
Celalettin Calis <celalettin.calis@chronosphere.io>
2025-07-09 13:59:38 +0300

    change buffering strategy
    Signed-off-by: Celalettin Calis <celalettin.calis@chronosphere.io>

--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
+package s3client
+
 import (
 	"archive/tar"
-	"bytes"
+	"bufio"
 	"compress/gzip"
 	"errors"
 	"io"
@@ -9,6 +11,29 @@ import (
 	"strings"
 )
 
+// IsBinaryContentType returns true if the given content type is a binary content type,
+// and false otherwise.
+func IsBinaryContentType(contentType string) bool {
+	// Parse the content type to get the media type and parameters
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		// If the content type is invalid, assume it is not binary
+		return false
+	}
+
+	// Check if the media type is in the list of known binary media types
+	switch mediaType {
+	case "application/octet-stream", "application/gzip", "application/x-tar", "application/tar+gzip":
+		return true
+	default:
+		return false
+	}
+}
+
+// GetFileReader returns a function that creates a reader for a given file,
+// based on the file's extension.
+// The returned function takes an io.Reader as input and returns an io.Reader
+// and an error, if any.
 func GetFileReader(filename string) func(io.Reader) (io.ReadCloser, error) {
 	// Get the file extension of the given file
 	extension := strings.ToLower(filepath.Ext(filename))
@@ -17,28 +42,27 @@ func GetFileReader(filename string) func(io.Reader) (io.ReadCloser, error) {
 	switch {
 	case extension == ".gz" || extension == ".gzip":
 		return func(r io.Reader) (io.ReadCloser, error) {
-			// We need to buffer the data because gzip.NewReader consumes bytes
-			// to check the header, and if it fails, we need the original data
-			body, err := io.ReadAll(r)
-			if err != nil {
+			// Use a buffered reader to peek at the header without consuming bytes
+			br := bufio.NewReader(r)
+			
+			// Peek at the first few bytes to check if it's actually gzipped
+			header, err := br.Peek(3)
+			if err != nil && err != io.EOF {
 				return nil, err
 			}
-
-			// Try to create a gzip reader from the buffered data
-			gr, err := gzip.NewReader(bytes.NewReader(body))
-			if err != nil {
-				// See https://github.com/aws/aws-sdk-go/issues/1292
-				// The default HTTP transports that the AWS SDK uses will decompress objects transparently
-				// if the Content Encoding is gzip. Not everyone or everything properly sets the Content-Encoding
-				// header on their S3 objects, so we could be trying to process gzipped objects and not know it.
-				if errors.Is(err, gzip.ErrHeader) {
-					// If it's not actually gzipped, return the original buffered data
-					rc := io.NopCloser(bytes.NewReader(body))
-					return rc, nil
+			
+			// Check for gzip magic number (0x1f, 0x8b)
+			if len(header) >= 2 && header[0] == 0x1f && header[1] == 0x8b {
+				// It's actually gzipped, create streaming gzip reader
+				gr, err := gzip.NewReader(br)
+				if err != nil {
+					return nil, err
 				}
-				return nil, err
+				return gr, nil
+			} else {
+				// Not actually gzipped, return the buffered reader as-is
+				return io.NopCloser(br), nil
 			}
-			return gr, nil
 		}
 	case extension == ".tar":
 		return func(r io.Reader) (io.ReadCloser, error) {
@@ -51,4 +75,32 @@ func GetFileReader(filename string) func(io.Reader) (io.ReadCloser, error) {
 			return rc, nil
 		}
 	}
+}
+
+// IsGlobPattern returns true if the given string is a glob pattern.
+func IsGlobPattern(s string) bool {
+	// Check if the string contains any of the special glob characters: *, ?, [, or \
+	return strings.ContainsAny(s, "*?[\\")
+}
+
+// GetDirPrefix returns the directory prefix from a glob expression.
+func GetDirPrefix(glob string) string {
+	// Split the glob expression by the path separator
+	parts := strings.Split(glob, string(filepath.Separator))
+
+	// Find the index of the last wildcard
+	lastWildcardIndex := -1
+	for i := len(parts) - 1; i >= 0; i-- {
+		if strings.ContainsAny(parts[i], "*?[") {
+			lastWildcardIndex = i
+		}
+	}
+
+	// If there is a wildcard, return the part before it
+	if lastWildcardIndex >= 0 {
+		return strings.Join(parts[:lastWildcardIndex], string(filepath.Separator))
+	}
+
+	// If there are no wildcards, the whole expression is the directory prefix
+	return glob
 }
